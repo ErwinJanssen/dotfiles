679caf57052b30d92da7d3bbac40b355a82a13c5
Erwin Janssen <erwinjanssen@outlook.com>
2019-08-27 13:07:15 +0200

    Fish: Color prompt based on $status_code

--- fish/files/fish/functions/fish_prompt.fish
+++ fish/files/fish/functions/fish_prompt.fish
@@ -2,8 +2,14 @@ set __fish_git_prompt_show_informative_status 'yes'
 set __fish_git_prompt_showcolorhints 'yes'
 
 function fish_prompt --description 'Write out the prompt'
+    set --local exit_code $status
+
     if not set -q __fish_prompt_normal
         set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_prompt_error
+        set -g __fish_prompt_error (set_color red)
     end
 
     if not set -q __fish_prompt_cwd
@@ -16,5 +22,11 @@ function fish_prompt --description 'Write out the prompt'
     if functions -q __fish_vcs_prompt
         echo -ns (__fish_vcs_prompt)
     end
-    echo -ns "$__fish_prompt_normal> "
+
+    if test $exit_code -eq 0
+        echo -ns "$__fish_prompt_normal> "
+    else
+        echo -ns "$__fish_prompt_error> "
+    end
+
 end
