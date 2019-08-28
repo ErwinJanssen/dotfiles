650361a6a27481eab5d497c092849333ac470f62
Erwin Janssen <erwinjanssen@outlook.com>
2019-08-28 08:17:38 +0200

    Fish: Reduce size of `fish_prompt` function

--- fish/files/fish/functions/fish_prompt.fish
+++ fish/files/fish/functions/fish_prompt.fish
@@ -4,29 +4,21 @@ set __fish_git_prompt_showcolorhints 'yes'
 function fish_prompt --description 'Write out the prompt'
     set --local exit_code $status
 
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
+    echo -ns (set_color $fish_color_user) $USER
+    echo -ns (set_color normal) @
+    echo -ns (set_color $fish_color_host) (prompt_hostname)
+    echo -ns (set_color $fish_color_cwd) " " (prompt_pwd)
+    echo -ns (set_color normal)
 
-    if not set -q __fish_prompt_error
-        set -g __fish_prompt_error (set_color red)
-    end
-
-    if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
-
-    echo -ns "$USER" @ (prompt_hostname)
-    echo -ns " $__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
-    if functions -q __fish_vcs_prompt
+    if functions --query __fish_vcs_prompt
         echo -ns (__fish_vcs_prompt)
     end
 
     if test $exit_code -eq 0
-        echo -ns "$__fish_prompt_normal> "
+        set_color normal
     else
-        echo -ns "$__fish_prompt_error> "
+        set_color blue
     end
+    echo -ns "> "
 
 end
