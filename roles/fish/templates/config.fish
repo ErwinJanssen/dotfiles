b796a31780538a8d22b20e589016bebc2324aa78
Erwin Janssen <erwin.janssen@combell.group>
2022-10-21 10:07:13 +0200

    [fish] Merge history after every command
    Automatically merge the history after executing a command. This creates
    a global history that is shared between all fish sessions, and prevents
    entries from suddenly being "forgotten".

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -36,6 +36,14 @@ function fishprompt_newline \
     echo
 end
 
+# Automatically merge the history after executing a command. This creates
+# a global history that is shared between all fish sessions, and prevents
+# entries from suddenly being "forgotten".
+function global_history \
+        --description "Merge history after every command." \
+        --on-event fish_prompt
+    history merge
+end
 
 # Start Sway on tty1
 if [ (tty) = "/dev/tty1" ]
