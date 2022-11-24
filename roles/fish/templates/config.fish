dc65789deaf294a107e8c5d7bfaa7cae2f4c89c4
Erwin Janssen <erwinjanssen@outlook.com>
2022-11-24 10:27:33 +0100

    [fish] Send Sway's output to journalctl

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -50,5 +50,7 @@ starship init fish | source
 
 # Start Sway on tty1
 if [ (tty) = "/dev/tty1" ]
-    exec sway
+    # Send Sway's output to journald, retrieve with:
+    # `journalctl --user --identifier sway`
+    exec systemd-cat --identifier=sway sway
 end
