fbdc938703a1b548d12e85834f6182423f1bb543
Erwin Janssen <erwin.janssen@combell.group>
2023-05-11 13:23:54 +0200

    [neovim] Disable combined TS/regex highlighting
    The tree-sitter highlighting plugin has an option to combine it with the
    traditional regex based highlighting. This was enabled, because
    tree-sitter highlighting on its own caused some issues. These problems
    no longer seem to exist. In fact, the combined highlighting results in
    some visual noise, with some words having two different colors. Disable
    this setting, and only use tree-sitter highlighting.

--- roles/neovim/templates/lua/plugins/treesitter.lua
+++ roles/neovim/templates/lua/plugins/treesitter.lua
@@ -12,17 +12,5 @@ require("nvim-treesitter.configs").setup {
         -- highlighting colors the characters of the subject line red after
         -- 50 characters.
         disable = { "gitcommit" },
-        -- Enable both tree-sitter highlighting and Vim regex highlighting.
-        -- Some reasons to do this:
-        -- -    For some file types, this provides a better highlighting
-        --      experience (e.g. Ansible configuration files in YAML that also
-        --      contain Jinja2 templating).
-        -- -    Another important reason is spell checking. Vim regex
-        --      highlighting provides a `@NoSpell` annotation on highlight
-        --      groups that prevent things like variable names from being
-        --      marked as a spelling error. Tree-sitter doesn't support this,
-        --      resulting in a lot of visual noise. Enabling Vim regex
-        --      highlighting alongside tree-sitter prevents these issues.
-        additional_vim_regex_highlighting = true,
     },
 }
