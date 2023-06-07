c020e71f785b5b13c31f4f6b88a2eca4ff51711e
Erwin Janssen <erwin.janssen@combell.group>
2023-06-07 12:23:00 +0200

    [neovim] Set tab width of gitcommit filetype to 4
    Instead a hardcoding a tab width of 4, `vim.opt_global.*:get()` was used
    to retrieve the tab width value. This seemed to work at first, but it no
    longer works anymore: `vim.opt_global.tabstop:get()` now returns the
    value 8.
    
    Revert back to using hardcoded values.

--- roles/neovim/templates/after/ftplugin/gitcommit.lua
+++ roles/neovim/templates/after/ftplugin/gitcommit.lua
@@ -1,6 +1,6 @@
 -- The recommended line length of gitcommit messages is 72 characters
 vim.opt_local.textwidth = 72
 
--- Change the width of a tab back to 4, as specified in the main configuration
-vim.opt_local.shiftwidth = vim.opt_global.shiftwidth:get()
-vim.opt_local.tabstop = vim.opt_global.tabstop:get()
+-- Change the width of a tab back to 4
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
