7227e33ac437e519bd4e59153b4e0d97a38d7268
Erwin Janssen <erwin.janssen@combell.group>
2023-12-06 12:09:43 +0100

    [guix] Add initial Guix Home configuration
    This file can be passed to `guix home reconfigure` to setup
    configuration files in the home directory:
    
    ```sh
    guix home reconfigure guix/home-configuration.scm
    ```
    
    An empty `home-configuration.scm` will cause Guix to do nothing, except
    configure Fontconfig. The minimal example on which to build, should thus
    include the desired Fontconfig setup; Fira Code as preferred monospace
    font.

---
+++ guix/home-configuration.scm
@@ -0,0 +1,29 @@
+;; This "home-environment" file can be passed to 'guix home reconfigure'
+;; to reproduce the content of the Home profile.
+;;
+;; Note from `guix import`:
+;;
+;;  "This is "symbolic": it only specifies package names. To reproduce the
+;;  exact same profile, you also need to capture the channels being used, as
+;;  returned by "guix describe". See the "Replicating Guix" section in the
+;;  manual.
+
+(use-modules
+  (gnu home)
+  (gnu packages)
+  (gnu services)
+  (gnu home services fontutils)
+  )
+
+(home-environment
+  ;; These packages will be installed and show up in the 'Home' profile, under
+  ;; `~/.guix-home/profile`.
+  (packages (specifications->packages (list
+    ;; Preferred monospace font
+    "font-fira-code"
+  )))
+
+  ;; Services used to configure the Home profile
+  (services (list
+    (load "services/fontconfig.scm")
+  )))
