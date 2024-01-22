9d184367a31c88f7b5ec7a7ba87aa2b06948747e
Erwin Janssen <erwin.janssen@combell.group>
2024-01-22 16:12:42 +0100

    [wezterm] Disable tab bar

--- roles/terminals/wezterm/templates/wezterm.lua
+++ roles/terminals/wezterm/templates/wezterm.lua
@@ -1,5 +1,8 @@
 -- This table will hold the configuration.
 local config = {}
 
+-- No tab bar is needed, this is either done via Sway or tmux.
+config.enable_tab_bar = false
+
 -- Return the configuration to wezterm.
 return config
