0c83c002865f1fdaea6243d537f0ec7b0f455ae3
Erwin Janssen <erwinjanssen@outlook.com>
2024-02-11 11:54:10 +0100

    [tmux] Fix wrong service name in tmux/service.scm

--- guix/services/tmux/service.scm
+++ guix/services/tmux/service.scm
@@ -1,5 +1,5 @@
 (add-user-package "tmux")
 
-(add-user-service (simple-service 'ripgrep-config
+(add-user-service (simple-service 'tmux-config
   home-files-service-type
   `((".tmux.conf" ,(local-file "generated/tmux.conf")))))
