df24c20370decc7e57714d1e46d6fadb2f3a1b60
Erwin Janssen <erwinjanssen@outlook.com>
2024-02-11 12:13:21 +0100

    [i3status] Move i3status to Sway Guix service

--- guix/services/sway/service.scm
+++ guix/services/sway/service.scm
@@ -1,5 +1,11 @@
+(add-user-package "i3status")
+
 (add-user-service (simple-service 'sway-fish-config
   home-fish-service-type
   (home-fish-extension
     (config (list (local-file "config.fish"
               ))))))
+
+(add-user-service (simple-service 'i3status-config
+  home-xdg-configuration-files-service-type
+  `(("i3status/config" ,(local-file "generated/i3status/config")))))
