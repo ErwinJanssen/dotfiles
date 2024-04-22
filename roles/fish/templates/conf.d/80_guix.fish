297f9b9ab67c288ed5aba8285214e04ad42502c7
Erwin Janssen <erwinjanssen@outlook.com>
2024-04-22 18:17:21 +0200

    [fish] Remove Guix setup

--- roles/fish/templates/conf.d/80_guix.fish
+++
@@ -1,5 +0,0 @@
-# Set and export the Guix profile path, as recommended by Guix itself.
-set --global --export GUIX_PROFILE "$HOME/.config/guix/current"
-
-# Add the bin directory of the current Guix profile to `$PATH`.
-fish_add_path --global "$GUIX_PROFILE/bin"
