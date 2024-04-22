384799619b7e6b6aab5af05307d2a2dbd1d50fa8
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 13:42:45 +0200

    [fish] Move custom keybindings to Nix config

--- nix/home-manager/programs/fish/interactive_shell_init.fish
+++ nix/home-manager/programs/fish/interactive_shell_init.fish
@@ -1,6 +1,15 @@
 # Disable greeting message at start
 set fish_greeting
 
+# Custom key bindings
+function fish_user_key_bindings
+    # ctrl-del
+    bind \e\[3\;5~ kill-word
+
+    # ctrl-backspace
+    bind \cH backward-kill-word
+end
+
 # Start Sway on tty1
 if [ (tty) = /dev/tty1 ]
     # Send Sway's output to journald, retrieve with:
--- roles/fish/templates/conf.d/10_base.fish
+++
@@ -1,8 +0,0 @@
-# Custom key bindings
-function fish_user_key_bindings
-    # ctrl-del
-    bind \e\[3\;5~ kill-word
-
-    # ctrl-backspace
-    bind \cH backward-kill-word
-end
