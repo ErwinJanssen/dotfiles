4badbb33b759910bd524260f836d86934122bd2f
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 11:12:12 +0200

    [fish] Do not let Fish set `SSH_AUTH_SOCK`
    This environment variable is now set by Nix Home Manager for all shells,
    no need to add it to the Fish configuration as well.

--- roles/fish/templates/conf.d/10_base.fish
+++ roles/fish/templates/conf.d/10_base.fish
@@ -4,8 +4,6 @@ set fish_greeting
 # Preferred tools
 set --global --export BROWSER {{ browser }}
 
-set --global --export SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-
 # Custom key bindings
 function fish_user_key_bindings
     # ctrl-del
