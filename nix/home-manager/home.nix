f858cdbebdb1539925736054a4fce0419f45af06
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 16:33:33 +0200

    [bat] Install with Nix instead of Ansible

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -31,6 +31,9 @@ in
     pkgs.fd
     pkgs.fira-code
 
+    # fzf.vim automatically uses `bat` for file previews if it is available.
+    pkgs.bat
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
