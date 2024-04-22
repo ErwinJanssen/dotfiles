20140a570a03b532f112102bed3f55efb5d97bf7
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 11:53:47 +0200

    [fish] Remove conditional around `eza` alias
    This program is guaranteed to be installed, no need to check it. If it
    does not exist, something in the setup is going wrong.

--- roles/fish/templates/conf.d/20_aliases.fish
+++ roles/fish/templates/conf.d/20_aliases.fish
@@ -1,7 +1,4 @@
-# Aliases
-if command --search eza >/dev/null
-    # eza is a modern ls replacement, use it if available
-    function ls --description "List contents of directory"
-        command eza $argv
-    end
+# eza is a modern ls replacement, use it if available
+function ls --description "List contents of directory"
+    command eza $argv
 end
