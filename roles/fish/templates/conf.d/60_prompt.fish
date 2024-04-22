310ea848046b338c34442b69cb3eb2c3d1d1562c
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 11:04:16 +0200

    [fish] Remove `fishprompt_newline` function
    It seems as if this adds more newlines that necessary. Similar
    functionality can also be achieved through Starship prompt config if
    needed.

--- roles/fish/templates/conf.d/60_prompt.fish
+++ roles/fish/templates/conf.d/60_prompt.fish
@@ -1,11 +1,3 @@
-# Insert a new line before displaying the prompt. This separates the command
-# prompt from the previous output.
-function fishprompt_newline \
-    --description "Insert a new line before displaying the prompt." \
-    --on-event fish_prompt
-    echo
-end
-
 # Automatically merge the history after executing a command. This creates
 # a global history that is shared between all fish sessions, and prevents
 # entries from suddenly being "forgotten".
