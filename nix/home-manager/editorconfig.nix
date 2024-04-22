f1eaeea834898d82ae2ab0a39766e2ed918a2fc2
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 16:56:18 +0200

    [editorconfig] insert_final_newline = true

--- nix/home-manager/editorconfig.nix
+++ nix/home-manager/editorconfig.nix
@@ -7,6 +7,9 @@
       # Trim trailing whitespace on every line. Does not touch trailing empty
       # newlines at the end of the file.
       trim_trailing_whitespace = true;
+
+      # Ensure at least one final newline is always present.
+      insert_final_newline = true;
     };
   };
 }
