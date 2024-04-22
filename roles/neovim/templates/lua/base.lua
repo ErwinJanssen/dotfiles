a3899eb74e8bc699748f29aa960632c1ae4f6c03
Erwin Janssen <erwin.janssen@team.blue>
2024-04-22 17:05:57 +0200

    [neovim] Set split direction in `base.lua`

--- roles/neovim/templates/lua/base.lua
+++ roles/neovim/templates/lua/base.lua
@@ -3,3 +3,10 @@
 -- Space key doesn't have a purpose. Using Space is convenient, as it is
 -- located in the middle of the keyboard, and can be reached by both hands.
 vim.g.mapleader = " "
+
+-- When creating a (vertical) split, create this to the right or below of the
+-- current buffer (instead of to the left or above). The benefit is that the
+-- current buffer remains at the same place (and is not moved to the other part
+-- of the screen), which is easier on the eyes and might be more intuitive.
+vim.o.splitright = true
+vim.o.splitbelow = true
