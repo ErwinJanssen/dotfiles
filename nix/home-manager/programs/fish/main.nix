e97c11996a40cdd8d86347f1663f930db26071f7
Erwin Janssen <erwin.janssen@team.blue>
2024-04-23 17:08:25 +0200

    [fish] Prepend values to `$PATH` via Nix

--- nix/home-manager/programs/fish/main.nix
+++ nix/home-manager/programs/fish/main.nix
@@ -1,5 +1,6 @@
 {
   enable = true;
+  shellInit = builtins.readFile ./shell_init.fish;
   interactiveShellInit = builtins.readFile ./interactive_shell_init.fish;
   shellAliases = {
     ls = "eza";
