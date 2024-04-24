97d2277b132d81e12f3bc32467929de475f85bec
Erwin Janssen <erwin.janssen@team.blue>
2024-04-24 10:27:37 +0200

    [fish] Add `global_history` to config via Nix

--- nix/home-manager/programs/fish/interactive_shell_init.fish
+++ nix/home-manager/programs/fish/interactive_shell_init.fish
@@ -6,6 +6,15 @@
 # Disable greeting message at start
 set fish_greeting
 
+# Automatically merge the history after executing a command. This creates
+# a global history that is shared between all fish sessions, and prevents
+# entries from suddenly being "forgotten".
+function global_history \
+    --description "Merge history after every command." \
+    --on-event fish_prompt
+    history merge
+end
+
 # }}}
 
 #======================================
--- roles/fish/templates/conf.d/60_prompt.fish
+++
@@ -1,8 +0,0 @@
-# Automatically merge the history after executing a command. This creates
-# a global history that is shared between all fish sessions, and prevents
-# entries from suddenly being "forgotten".
-function global_history \
-    --description "Merge history after every command." \
-    --on-event fish_prompt
-    history merge
-end
