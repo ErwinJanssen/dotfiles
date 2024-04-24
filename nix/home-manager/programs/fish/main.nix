9bd0cdb4f2858ad477024a7218192f5272b35705
Erwin Janssen <erwin.janssen@team.blue>
2024-04-24 10:21:35 +0200

    [fish] Use Nix to create `print_fish_colors`

--- nix/home-manager/programs/fish/main.nix
+++ nix/home-manager/programs/fish/main.nix
@@ -2,6 +2,12 @@
   enable = true;
   shellInit = builtins.readFile ./shell_init.fish;
   interactiveShellInit = builtins.readFile ./interactive_shell_init.fish;
+  functions = {
+    print_fish_colors = {
+      description = "Shows the various fish colors being used";
+      body = builtins.readFile ./functions/print_fish_colors.fish;
+    };
+  };
   shellAliases = {
     ls = "eza";
   };
