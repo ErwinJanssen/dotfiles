705fc85c6be3131f19291a8046eacc44f6134bdb
Erwin Janssen <erwin.janssen@team.blue>
2024-04-25 11:39:17 +0200

    Add minimal `flake.nix` and `flake.lock`

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  description = "Personal home configuration of Erwin Janssen";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }: { };
+}
