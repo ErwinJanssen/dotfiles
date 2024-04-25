e5549f77aec26a7d5b87a2e0c032bd01e8c4ead4
Erwin Janssen <erwin.janssen@team.blue>
2024-04-25 11:45:25 +0200

    Use `nixpkgs-unstable` as `flake.nix` input
    With the lockfile, it is easier to follow the rolling 'unstable'
    channel, without the need to re-fetch the nixpkgs archive every time.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Personal home configuration of Erwin Janssen";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
   };
 
   outputs = { self, nixpkgs }: { };
