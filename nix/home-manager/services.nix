38d56e3349de3afb5115b585dc89d7a3e22df647
Erwin Janssen <erwinjanssen@outlook.com>
2024-04-27 22:10:23 +0200

    [swayidle] Add dedicated service with Nix

--- nix/home-manager/services.nix
+++ nix/home-manager/services.nix
@@ -8,4 +8,5 @@
   network-manager-applet = import ./services/network-manager-applet.nix;
   nextcloud-client = import ./services/nextcloud-client.nix;
   ssh-agent = import ./services/ssh-agent.nix;
+  swayidle = import ./services/swayidle.nix;
 }
