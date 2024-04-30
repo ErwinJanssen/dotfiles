d306e48dc94bc4e00299bb4d9ca678f949022f1d
Erwin Janssen <erwinjanssen@outlook.com>
2024-04-30 21:01:28 +0200

    [waybar] Add `clock` module to `modules-right`

--- nix/home-manager/programs/waybar/main.nix
+++ nix/home-manager/programs/waybar/main.nix
@@ -8,6 +8,17 @@
   settings = {
     mainBar = {
       position = "top";
+      modules-right = [ "clock" ];
+      clock = {
+        # Show the time in 24h format.
+        format = "{:%T}";
+
+        # Refresh every second.
+        interval = 1;
+
+        # On hover, show the date and a small calendar of the current month.
+        tooltip-format = "<big>{:%Y-%m-%d}</big>\n<tt><small>{calendar}</small></tt>";
+      };
     };
   };
 }
