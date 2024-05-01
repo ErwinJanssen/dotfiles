acd9093378c73f890cfbc3fa5fcef0a0715bc734
Erwin Janssen <erwinjanssen@outlook.com>
2024-05-01 10:27:54 +0200

    [waybar] Add `memory` module to `modules-right`

--- nix/home-manager/programs/waybar/main.nix
+++ nix/home-manager/programs/waybar/main.nix
@@ -8,7 +8,10 @@
   settings = {
     mainBar = {
       position = "top";
-      modules-right = [ "clock" ];
+      modules-right = [
+        "memory"
+        "clock"
+      ];
       clock = {
         # Show the time in 24h format.
         format = "{:%T}";
@@ -18,6 +21,24 @@
 
         # On hover, show the date and a small calendar of the current month.
         tooltip-format = "<big>{:%Y-%m-%d}</big>\n<tt><small>{calendar}</small></tt>";
+      };
+
+      # Current memory usage.
+      memory = {
+        # By default, show memory usage as a percentage.
+        format = "{percentage}% ";
+
+        # Show actual usage values on hover.
+        tooltip-format = "Usage: {used:0.1f} / {total:0.1f} GiB";
+
+        # Refresh every 5 seconds.
+        interval = 5;
+
+        # Set thresholds for warning/error colors.
+        states = {
+          warning = 80;
+          critical = 90;
+        };
       };
     };
   };
