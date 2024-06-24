8856ec094922e092bf9c174a7477b67cae536d50
Erwin Janssen <erwinjanssen@outlook.com>
2024-06-24 18:52:19 +0200

    Add Home Manager as Nix Flake input
    This allows for pinning the version of Home Manager, making it more
    reproducible and easier to update.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+    }:
     {
       formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     };
