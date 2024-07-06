ae55189ee8e904c4624832f314aa321bc7113946
Erwin Janssen <erwinjanssen@outlook.com>
2024-07-06 10:57:50 +0200

    [git] Let Git only use SSH config from home dir

--- nix/home-manager/programs/git.nix
+++ nix/home-manager/programs/git.nix
@@ -6,6 +6,12 @@
     init = {
       defaultBranch = "main";
     };
+    core = {
+      # Explicitly use the SSH config found in the home directory. This
+      # prevents SSH from using the system wide configuration, which makes it
+      # more reproducible across devices.
+      sshCommand = "ssh -F ~/.ssh/config";
+    };
   };
   delta = {
     enable = true;
