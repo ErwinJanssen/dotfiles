f177b8016980965d35494d95e942b91c32d8f105
Erwin Janssen <erwinjanssen@outlook.com>
2024-08-14 20:43:11 +0200

    [nextcloud-client] Enable Home Manager service

--- nix/home-manager/services.nix
+++ nix/home-manager/services.nix
@@ -6,5 +6,6 @@
   };
   kanshi = import ./services/kanshi.nix;
   network-manager-applet = import ./services/network-manager-applet.nix;
+  nextcloud-client = import ./services/nextcloud-client.nix;
   ssh-agent = import ./services/ssh-agent.nix;
 }
