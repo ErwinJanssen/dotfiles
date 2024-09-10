37c6e473e4faeb5e91d69cee49a043def0991b66
Erwin Janssen <erwin.janssen@team.blue>
2024-09-10 12:52:01 +0200

    [neovim] Change mapping for grep with input to fG
    This frees up `<leader>fg` for something that will used more often.

--- nix/home-manager/programs/neovim/config/lua/mappings.lua
+++ nix/home-manager/programs/neovim/config/lua/mappings.lua
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<Leader>fL", "<cmd>Lines<CR>", {
     desc = "Find line in *any* buffer",
 })
 
-vim.keymap.set("n", "<Leader>fg", function()
+vim.keymap.set("n", "<Leader>fG", function()
     vim.cmd("Rg " .. vim.fn.input "Search string: ")
 end, { desc = "Find with ripgrep" })
 
