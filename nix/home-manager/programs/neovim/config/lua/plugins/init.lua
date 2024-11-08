f28e296b7cdc0780ea83cc864a957bb1ccdb0508
Erwin Janssen <erwin.janssen@team.blue>
2024-11-08 12:51:36 +0100

    [neovim] Remove checks for plugin existence
    Since all plugins are now installed with Nix Home Manager, there is no
    need to check for their existence.

--- nix/home-manager/programs/neovim/config/lua/plugins/init.lua
+++ nix/home-manager/programs/neovim/config/lua/plugins/init.lua
@@ -37,62 +37,46 @@ require("telescope").setup {
     },
 }
 
--- Run initialization for plugins if they are installed
-local gitsigns_ok, gitsigns = pcall(require, "gitsigns")
-if gitsigns_ok then
-    gitsigns.setup()
-end
-
-local whichkey_ok, whichkey = pcall(require, "wich-key")
-if whichkey_ok then
-    whichkey.setup()
-end
+require("gitsigns").setup()
+require("which-key").setup()
 
 require "plugins.cmp"
 
-local treesitter_ok, _ = pcall(require, "nvim-treesitter.configs")
-if treesitter_ok then
-    require "plugins.treesitter"
-end
+require "plugins.treesitter"
 
-local mason_ok, mason = pcall(require, "mason")
-if mason_ok then
-    mason.setup()
-end
+require("mason").setup()
 
-local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-if mason_lspconfig_ok then
-    -- Setup both `mason` and `lspconfig` via `mason-lspconfig`. Ensure certain
-    -- packages are installed, and provide a setup handler that automatically
-    -- registers language servers with `lspconfig`.
-    mason_lspconfig.setup {
-        ensure_installed = {
-            "ansiblels",
-            "bashls",
-            "lua_ls",
-            "nil_ls",
-            "pyright",
-            "rust_analyzer",
-        },
-    }
-    mason_lspconfig.setup_handlers {
-        -- The first entry (without a key) will be the default handler
-        -- and will be called for each installed server that doesn't have
-        -- a dedicated handler.
-        function(server_name) -- default handler (optional)
-            require("lspconfig")[server_name].setup {}
-        end,
-    }
+local mason_lspconfig = require "mason-lspconfig"
 
-    -- Language specific configurations
-    require("lspconfig").lua_ls.setup {
-        settings = {
-            Lua = {
-                diagnostics = {
-                    -- Suppress "Undefined global `vim`" error
-                    globals = { "vim" },
-                },
+-- Setup both `mason` and `lspconfig` via `mason-lspconfig`. Ensure certain
+-- packages are installed, and provide a setup handler that automatically
+-- registers language servers with `lspconfig`.
+mason_lspconfig.setup {
+    ensure_installed = {
+        "ansiblels",
+        "bashls",
+        "lua_ls",
+        "nil_ls",
+        "pyright",
+        "rust_analyzer",
+    },
+}
+mason_lspconfig.setup_handlers {
+    -- The first entry (without a key) will be the default handler and will be
+    -- called for each installed server that doesn't have a dedicated handler.
+    function(server_name) -- default handler (optional)
+        require("lspconfig")[server_name].setup {}
+    end,
+}
+
+-- Language specific configurations
+require("lspconfig").lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Suppress "Undefined global `vim`" error
+                globals = { "vim" },
             },
         },
-    }
-end
+    },
+}
