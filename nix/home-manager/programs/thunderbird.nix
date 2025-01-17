fc468b13ef2b46630138b1097187749c34b4e0b1
Erwin Janssen <erwin.janssen@team.blue>
2025-01-17 14:01:07 +0100

    [thunderbird] Set default sort settings

--- nix/home-manager/programs/thunderbird.nix
+++ nix/home-manager/programs/thunderbird.nix
@@ -8,6 +8,16 @@
   profiles = {
     default = {
       isDefault = true;
+      settings = {
+        # Sort by date.
+        "mailnews.default_sort_type" = 18;
+
+        # Descending sort by default, show most recent first.
+        "mailnews.default_sort_order" = 2;
+
+        # Disable threads.
+        "mailnews.default_view_flags" = 0;
+      };
     };
   };
 }
