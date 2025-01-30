d3cdccaf1f67c34fb1aa5555a6014d5acf20f676
Erwin Janssen <erwin.janssen@team.blue>
2025-01-30 12:11:43 +0100

    [neovim] Enable auto-format of Markdown text
    This is an experiment, to see if this works properly.

--- nix/home-manager/programs/neovim/config/after/ftplugin/markdown.lua
+++ nix/home-manager/programs/neovim/config/after/ftplugin/markdown.lua
@@ -2,6 +2,10 @@
 vim.opt_local.formatoptions:append {
     -- Auto-wrap text using 'textwidth'.
     t = true,
+
+    -- Automatic formatting of paragraphs. Every time text is inserted or
+    -- deleted the paragraph will be reformatted.
+    a = true,
 }
 
 -- Enable checking for words that are not capitalized, by resetting it to the
