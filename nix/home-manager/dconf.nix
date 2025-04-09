bd4f61d21b10167ce20e33a3ed5c81b8cbd6f419
Erwin Janssen <erwinjanssen@outlook.com>
2025-04-10 01:04:07 +0200

    [dconf] Add touchpad settings

--- nix/home-manager/dconf.nix
+++ nix/home-manager/dconf.nix
@@ -1,3 +1,18 @@
 {
-  settings = { };
+  settings = {
+    "org/gnome/desktop/peripherals/touchpad" = {
+      # This seems to enable both 'areas' and 'tap to click'.
+      click-method = "areas";
+
+      # Prevent accidental mouse movements during typing.
+      disable-while-typing = true;
+
+      # Scroll the viewport, not the content.
+      natural-scroll = false;
+
+      # Scroll using two fingers, the alternative is dragging a single
+      # finger at the edge of the touchpad.
+      two-finger-scrolling-enabled = true;
+    };
+  };
 }
