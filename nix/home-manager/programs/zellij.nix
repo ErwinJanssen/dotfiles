c46b86d9472024dcf3e6e71cffb4c84bdf153df5
Erwin Janssen <erwinjanssen@outlook.com>
2025-07-08 13:18:42 +0200

    [zellij] Set `fish` as default shell

--- nix/home-manager/programs/zellij.nix
+++ nix/home-manager/programs/zellij.nix
@@ -6,4 +6,11 @@
   enableBashIntegration = false;
   enableFishIntegration = false;
   enableZshIntegration = false;
+
+  settings = {
+    # Default to starting `fish` if no shell is specified. This is mainly to
+    # prevent the need to manually run `fish` each time when Zellij is started
+    # using `nix develop`.
+    default_shell = "fish";
+  };
 }
