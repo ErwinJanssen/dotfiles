772c03ed23ffbc77f0d2c36bcc96fab4499dadfa
Mistral Vibe <vibe@mistral.ai>
2026-02-13 16:25:50 +0100

    [ssh] Disable default Home Manager supplied config

--- nix/home-manager/programs/ssh.nix
+++ nix/home-manager/programs/ssh.nix
@@ -1,4 +1,8 @@
 {
   enable = true;
   includes = [ "~/.ssh/config.d/*.conf" ];
+
+  # Disable default config as these values are not required. This suppresses
+  # the warning "`programs.ssh` default values will be removed in the future".
+  enableDefaultConfig = false;
 }
