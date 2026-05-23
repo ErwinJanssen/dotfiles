acb96cac87428136433281f7692ef05b934d9d0b
Erwin Janssen <erwinjanssen@outlook.com>
2026-05-24 00:44:30 +0200

    [zed-editor] Add support for Nix files

--- nix/home-manager/programs/zed-editor.nix
+++ nix/home-manager/programs/zed-editor.nix
@@ -3,6 +3,13 @@
   programs.zed-editor = {
     enable = true;
     package = config.lib.nixGL.wrap pkgs.zed-editor;
+    extraPackages = [
+      # Nix
+      pkgs.nixd
+    ];
+    extensions = [
+      "nix"
+    ];
     userSettings = {
       # Automatically save when changing focus.
       autosave = "on_focus_change";
