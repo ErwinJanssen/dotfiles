6ce75938b265a78af46e6d79575d4ca0e9e77957
Erwin Janssen <code@erwinjanssen.nl>
2026-06-11 21:13:06 +0200

    Include `sd` package

--- nix/home-manager/home.nix
+++ nix/home-manager/home.nix
@@ -79,6 +79,7 @@ in
     pkgs.bottom
     pkgs.gnumake
     pkgs.pre-commit
+    pkgs.sd # Modern `sed` alternative
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
