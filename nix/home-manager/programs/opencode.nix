df6a47c48c608a8f7253ea13e7be63e6cc48855f
Erwin Janssen <code@erwinjanssen.nl>
2026-06-25 13:24:02 +0200

    [opencode] Configure BlueAI

--- nix/home-manager/programs/opencode.nix
+++ nix/home-manager/programs/opencode.nix
@@ -1,6 +1,23 @@
 {
   programs.opencode = {
     enable = true;
+    settings = {
+      provider = {
+        # Configure username for the BlueAI provider, the authentication is in
+        # `~/.local/share/opencode/auth.json`.
+        #
+        # Additional configuration is automatically loaded from the upsteam
+        # `.well-known/opencode`.
+        blueai = {
+          options = {
+            headers = {
+              x-bluecode-user = "erwin.janssen@team.blue";
+              x-token-factory-user = "erwin.janssen@team.blue";
+              x-client-app-id = "opencode";
+            };
+          };
+        };
+      };
+    };
   };
-
 }
